d433b9c6e8cc3bb276b06749ca207677e2447dbf
Mikael Aboagye - WD Studios L.L.C <mike.amanfo12@gmail.com>
2023-12-03 22:33:56 -0500

    Rename Update

--- init.cmake
+++ init.cmake
@@ -1,13 +1,13 @@
 ### Assimp
-set (EZ_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset importer.")
-mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_SUPPORT)
+set (NS_3RDPARTY_ASSIMP_SUPPORT ON CACHE BOOL "Whether to add support for the asset importer.")
+mark_as_advanced(FORCE NS_3RDPARTY_ASSIMP_SUPPORT)
 
-set (EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT OFF CACHE BOOL "Whether to include Draco mesh compression support in AssImp.")
-mark_as_advanced(FORCE EZ_3RDPARTY_ASSIMP_DRACO_SUPPORT)
+set (NS_3RDPARTY_ASSIMP_DRACO_SUPPORT OFF CACHE BOOL "Whether to include Draco mesh compression support in AssImp.")
+mark_as_advanced(FORCE NS_3RDPARTY_ASSIMP_DRACO_SUPPORT)
 
-macro(ez_requires_assimp)
+macro(ns_requires_assimp)
 	
-	ez_requires(EZ_3RDPARTY_ASSIMP_SUPPORT)
-	ez_requires_one_of(EZ_CMAKE_PLATFORM_LINUX EZ_CMAKE_PLATFORM_WINDOWS_DESKTOP)
+	ns_requires(NS_3RDPARTY_ASSIMP_SUPPORT)
+	ns_requires_one_of(NS_CMAKE_PLATFORM_LINUX NS_CMAKE_PLATFORM_WINDOWS_DESKTOP)
 
 endmacro()
